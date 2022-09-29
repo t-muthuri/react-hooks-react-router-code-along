91f759698cf780b6434e7e9812cd577d7af53698
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-09-29 11:21:21 +0300

    Render home

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// Step 1. Import react-router functions
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//BrowserRouter component is the base for our application's routing
+//Route component handles the conditional rendering
 
 function Home() {
   return (
@@ -9,8 +13,26 @@ function Home() {
   );
 }
 
-function App() {
-  return <Home />;
-}
+// Step 2. Change so router is coordinating what is displayed
+ReactDOM.render(
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />}/>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// function Home() {
+//   return (
+//     <div>
+//       <h1>Home!</h1>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return <Home />;
+// }
+
+// ReactDOM.render(<App />, document.getElementById("root"));
