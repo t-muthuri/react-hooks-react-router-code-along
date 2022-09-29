475a8280e35cc39acfe26e622aeafd88d1074601
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-09-29 11:51:10 +0300

    Adding other paths

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 //BrowserRouter component is the base for our application's routing
 //Route component handles the conditional rendering
+//Switch gives more predictable behaviour to our roots but in v6 ROutes replaces it
 
 function Home() {
   return (
@@ -13,11 +14,38 @@ function Home() {
   );
 }
 
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
 // Step 2. Change so router is coordinating what is displayed
 ReactDOM.render(
   <BrowserRouter>
   <Routes>
     <Route path="/" element={<Home />}/>
+    <Route path="/about" element={<About />}/>
+    <Route path="/login" element={<Login />}/>
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")
